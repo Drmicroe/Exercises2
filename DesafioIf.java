8a1129d710882d97d5d8cba9886fd22482663328
Drmicroe <77365559+Drmicroe@users.noreply.github.com>
2022-06-07 17:27:05 -0300

    Add files via upload

---
+++ DesafioIf.java
@@ -0,0 +1,22 @@
+package Controle;
+
+public class DesafioIf {
+	
+	public static void main(String[] args) {
+		
+		double nota = 9;
+		
+		// não usar ponto e vírgula em estrutura if
+		if(nota >= 9.0) {
+			System.out.println("Quadro de Honra!");
+			System.out.println("Você é fera!!!");
+			
+		}
+		
+		if(nota <= 8) {
+			System.out.println("Não atingiu o objetivo");
+		
+		}
+	}
+
+}
